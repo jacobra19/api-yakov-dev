c126bb89bce414f3e445e8e25256d023ccc1385d
Yakov Rakhamimov <jacobra19@gmail.com>
2021-09-29 12:06:37 +0300

    removed dist folder and updated git ignore

--- build/routes/comics/getComics/getComics.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getComics = void 0;
-const getComics = (req, res) => {
-    return res.send('comics route');
-};
-exports.getComics = getComics;
